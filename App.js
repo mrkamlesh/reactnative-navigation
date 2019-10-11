597ae6675626db63308f0f6840b1c09cce5d14ce
Kamlesh Kumar <kamlesh.kumar@agreeya.com>
2019-10-11 15:44:15 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+} from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import ProductAddScreen from './components/ProductAddScreen';
+import ProductDetailsScreen from './components/ProductDetailsScreen';
+import ProductEditScreen from './components/ProductEditScreen';
+import ProductScreen from './components/ProductScreen';
+
+const RootStack = createStackNavigator(
+  {
+    Product: ProductScreen,
+    ProductDetails: ProductDetailsScreen,
+    AddProduct: ProductAddScreen,
+    EditProduct: ProductEditScreen,
+  },
+  {
+    initialRouteName: 'Product',
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#777777',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  },
+);
+
+const RootContainer = createAppContainer(RootStack);
+
+const App: () => React$Node = () => {
+  return (
+    <RootContainer />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
